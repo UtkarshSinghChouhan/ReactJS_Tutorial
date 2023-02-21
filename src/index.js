0af17c4eeabcbaf0bf3fa4f582d8203d0941eb87
UtkarshSinghChouhan <uscutkarsh9@gmail.com>
2023-02-21 09:09:13 +0530

    template-literal completed

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from'react';
-import ReactDOM from'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const firstName = "Utkarsh Singh Chouhan";
+const firstName = "Utkarsh";
+const LastName = "Singh";
+
+// Normally we use template literals in the Javascript  like this
+console.log(`My name is ${firstName} ${LastName}`)
 
 
-// JS in JSX 
+// Inside the JSX we can use the template literal like this
 ReactDOM.render(
     <>
-        <h1>My name is : {firstName}</h1>
+        <h1>My name is {firstName} {LastName}</h1>
 
-
-        <h1>My age is : 2023 - 1999</h1>
-        <h1>My age is : {2023 - 1999}</h1>
+        {/* template literals */}
+        <h1>{`My name is ${firstName} ${LastName}`}</h1>
+        <h1>{`My age is ${2023 - 1999}`}</h1>
     </>,
     document.getElementById("root")
 );
