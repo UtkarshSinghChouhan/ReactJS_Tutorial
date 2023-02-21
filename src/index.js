a118c19177740ed34a8415ed9e0edb2d8ea6d879
UtkarshSinghChouhan <uscutkarsh9@gmail.com>
2023-02-21 12:34:21 +0530

    External CSS Completed

--- src/index.js
+++ src/index.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 
 const name = "Utkarsh Singh Chouhan";
 
-const img1 = "https://picsum.photos/id/237/200/300";
-const img2 = "https://picsum.photos/id/237/250/300";
-const img3 = "https://picsum.photos/id/237/300/300";
+const img1 = "https://picsum.photos/200/300";
+const img2 = "https://picsum.photos/250/300";
+const img3 = "https://picsum.photos/300/300";
 
 const link = "https://in.pinterest.com/";
 
 ReactDOM.render(
     <>
-        <h1 contentEditable>{name}</h1>
-        <img src={img1}/>
-        <img src={img2}/>
+        <h1 className='heading'>{name}</h1>
 
-        <a href={link} target="_blank">
-            <img src={img3}/>
-        </a>
+        <div className='img_div'>
+            <img src={img1}/>
+            <img src={img2}/>
+
+            <a href={link} target="_blank">
+                <img src={img3}/>
+            </a>
+
+        </div>
     </>,
     document.getElementById("root")
 );
