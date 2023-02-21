b4c39a9fb13e609c93904d465fb4b3ec94c2cf42
UtkarshSinghChouhan <uscutkarsh9@gmail.com>
2023-02-21 07:31:09 +0530

    pure Javascript added

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM  from 'react-dom';
 
 ReactDOM.render(<h1>Hello World!!</h1>, document.getElementById("root"));
 
-
+// pure Javascript
 var h1 = document.createElement("h1");
 h1.innerHTML = "Utkarsh Singh Chouhan";
 document.getElementById("root").appendChild(h1);
