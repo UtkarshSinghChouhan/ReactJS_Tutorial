85fd248976f7b8ce9e09ac08463b74360c87a4ab
UtkarshSinghChouhan <uscutkarsh9@gmail.com>
2023-02-21 08:05:15 +0530

    react fragment completed

--- src/index.js
+++ src/index.js
@@ -2,29 +2,26 @@ import React  from 'react';
 import ReactDOM  from 'react-dom';
 
 
-// ADDING MUTIPLE JSX ELEMENT INSIDE THE render() function
 
-
-// In React v-16 it is possile for the render() function
-//  to return an array of elements
 
 // ReactDOM.render(
-//     [
-//         <h1>Hello World!!</h1>, 
-//         <h1>Hello World!!</h1>, 
-//         <h1>Hello World!!</h1>
-//     ],
+//    <React.Fragment>
+//     <h1>Hello World!!</h1>
+//     <h1>Hello World!!</h1>
+//     <h1>Hello World!!</h1>
+//    </React.Fragment>,
 //     document.getElementById("root")
 // );
 
-//  OR we can simple put the multiple elements inside the div tag
 
+// Syntactical Sugar form of the React.Fragment
 ReactDOM.render(
-   <div>
+   <>
     <h1>Hello World!!</h1>
     <h1>Hello World!!</h1>
     <h1>Hello World!!</h1>
-   </div>,
+   </>,
     document.getElementById("root")
 );
+
 
