e69cd20c3c594694e6da797d5b3f8627cd91e772
UtkarshSinghChouhan <uscutkarsh9@gmail.com>
2023-02-21 10:37:19 +0530

    Challenge-02 completed

--- src/index.js
+++ src/index.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const firstName = "Utkarsh";
-const LastName = "Singh";
-
-// Normally we use template literals in the Javascript  like this
-console.log(`My name is ${firstName} ${LastName}`)
+const name = "Utkarsh Singh Chouhan";
+const d = new Date();
 
 
-// Inside the JSX we can use the template literal like this
 ReactDOM.render(
     <>
-        <h1>My name is {firstName} {LastName}</h1>
-
-        {/* template literals */}
-        <h1>{`My name is ${firstName} ${LastName}`}</h1>
-        <h1>{`My age is ${2023 - 1999}`}</h1>
+        <h1>{`My name is ${name}`}</h1>
+        <h1>{`Date = ${d.toLocaleDateString()}`}</h1>
+        <h1>{`Time = ${d.toLocaleTimeString()}`}</h1>
     </>,
     document.getElementById("root")
 );
