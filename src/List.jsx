a4d8b747f09f00f02961116b4b69274fbc47edb9
UtkarshSinghChouhan <uscutkarsh9@gmail.com>
2023-02-23 06:43:30 +0530

    Fucntional Components completed

---
+++ src/List.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function List(){
+    return(
+        <ol>
+            <li>Utkarsh</li>
+            <li>Singh</li>
+            <li>Chouhan</li>
+        </ol>
+    );
+}
+
+export default List;
--- src/index.js
+++ src/index.js
@@ -1,33 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css'
+import App from './App';
 
-// For changing the style of the span dynamically
-let cssStyle = {
 
-};
-
-let greeting = "";
-
-let currDate = new Date();
-currDate = currDate.getHours();
-
-if(currDate >= 1 && currDate < 12){
-    greeting = "Good Morning";
-    cssStyle.color = "Green";
-}else if(currDate >= 12 && currDate < 19){
-    greeting = "Good AfterNoon";
-    cssStyle.color = "Orange";
-}else{
-    greeting = "Good Night"
-    cssStyle.color = "DarkBlue";
-}
-
-ReactDOM.render(
-    <>
-        <div>
-            <h1>Hello Sir, <span style={cssStyle}> {greeting} </span></h1>
-        </div>
-    </>,
-    document.getElementById("root")
-);
+ReactDOM.render(<App />,document.getElementById("root"));
