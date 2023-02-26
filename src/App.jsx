7cef5fab72fa3301e4774c3f575d1a450a3ff7c8
UtkarshSinghChouhan <uscutkarsh9@gmail.com>
2023-02-26 09:33:55 +0530

    ES import export module completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,18 @@
 import React from 'react';
-import './index.css';
 
-function App(){
-    // For changing the style of the span dynamically
-    let cssStyle = {
+const name = "Utkarsh";
 
-    };
-
-    let greeting = "";
-
-    let currDate = new Date();
-    currDate = currDate.getHours();
-
-    if(currDate >= 1 && currDate < 12){
-        greeting = "Good Morning";
-        cssStyle.color = "Green";
-    }else if(currDate >= 12 && currDate < 19){
-        greeting = "Good AfterNoon";
-        cssStyle.color = "Orange";
-    }else{
-        greeting = "Good Night"
-        cssStyle.color = "DarkBlue";
-    }
-
-    return(
-        <>
-            <div>
-                <h1>Hello Sir, <span style={cssStyle}> {greeting} </span></h1>
-            </div>
-        </>
-    );
+function lastName(){
+    const Lname = "Singh Chouhan";
+    return Lname;
 }
 
-export default App;
+const favLanguage = "React";
+
+function favProgram(){
+    const lan = "C++";
+    return lan;
+}
+
+export default name;
+export {lastName, favLanguage, favProgram};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+// import firstName, {lastName, favProgram, favLanguage} from './App';
 
 
+// ReactDOM.render(
+//     <>
+//         <ol>
+//             <li>{firstName}</li>
+//             <li>{lastName()}</li>
+//             <li>{favProgram()}</li>
+//             <li>{favLanguage}</li>
+//         </ol>
+//     </>,
+//     document.getElementById("root")
+// );
 
-ReactDOM.render(<App/>,document.getElementById("root"));
+
+// We can also import everything as an object
+import * as obj from './App';
+
+ReactDOM.render(
+    <>
+        <ol>
+            <li>{obj.default}</li>
+            <li>{obj.lastName()}</li>
+            <li>{obj.favProgram()}</li>
+            <li>{obj.favLanguage}</li>
+        </ol>
+    </>,
+    document.getElementById("root")
+);
