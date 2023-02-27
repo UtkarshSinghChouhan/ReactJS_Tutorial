acb87067e04d1292091cfac672d808262d74a90b
UtkarshSinghChouhan <uscutkarsh9@gmail.com>
2023-02-27 10:01:10 +0530

    Component inside component completed

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import Image from './Image';
+import Title from './Title';
 
 function Card(props){
     return(
         <>
             <div className='card'>
-                <img src={props.imgsrc} alt='' className='card_img'/>
-
+                <Image imgsrc={props.imgsrc}/>
+                
                 <div className='card_info'>
                     <span className='card_category'> A Netflix Original Series</span>
-                    <h3 className='card_title'> {props.title} </h3>
+                    <Title title={props.title}/>
                     <a href='' target='_blank'>
                         <button> Watch Now </button>
                     </a>
