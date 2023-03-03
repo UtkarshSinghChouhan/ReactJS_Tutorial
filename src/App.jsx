1e30f125798bffdc421418daa74c5b07b9d1e762
UtkarshSinghChouhan <uscutkarsh9@gmail.com>
2023-03-03 20:41:04 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,40 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import './index.css';
 
 const App = () => {
-    let [bgColor, setColor] = useState('salmon');
 
-    const changeColor = () => {
-        bgColor = 'gray';
-        setColor(bgColor);
-    };
+    let[message, updateMessage] = useState("");
+    let[fullMessage, updateFullMessage] = useState("");
 
-    const changeNewColor = () => {
-        bgColor = 'salmon';
-        setColor(bgColor);
+    const inputEvent = (e) => {
+        updateMessage(e.target.value);
+
+        // console.log(e);
+        // console.log(e.target);
+        // console.log(e.target.value);
+    }
+
+    const onSubmit = () => {
+        fullMessage = message;
+        updateFullMessage(fullMessage);
     }
 
     return(
         <>
-            <div className='container' style={{backgroundColor:bgColor}}>
-                <button onClick={changeColor} onDoubleClick={changeNewColor}>Click Me</button>
+            <div className='container'>
+                <h1>Hello {fullMessage}</h1>
+
+                <input 
+                    type='text' 
+                    placeholder='Enter Your Name' 
+                    onChange={inputEvent}
+                    value={message}
+                />
+
+                <button onClick={onSubmit}>Submit</button>
             </div>
         </>
     );
 }
 
-export default App;
+export default App; 
