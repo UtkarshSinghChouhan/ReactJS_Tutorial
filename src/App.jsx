81474775ebd49223fb581ba43e0b87732f08c83e
UtkarshSinghChouhan <uscutkarsh9@gmail.com>
2023-03-03 22:27:45 +0530

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -6,32 +6,52 @@ const App = () => {
     let[message, updateMessage] = useState("");
     let[fullMessage, updateFullMessage] = useState("");
 
+    let[lastName, updateLast] = useState("");
+    let[LastN, updateLastName] = useState("");
+
     const inputEvent = (e) => {
         updateMessage(e.target.value);
-
-        // console.log(e);
-        // console.log(e.target);
-        // console.log(e.target.value);
     }
 
-    const onSubmit = () => {
+    const inputEventTwo = (e) => {
+        updateLast(e.target.value);
+    }
+
+
+
+    const onSubmits = (e) => {
+        e.preventDefault();  //it will prevent the page from reloading while clicking the submit button
+
         fullMessage = message;
         updateFullMessage(fullMessage);
+
+        LastN = lastName;
+        updateLastName(LastN);
     }
 
     return(
         <>
             <div className='container'>
-                <h1>Hello {fullMessage}</h1>
+                <form onSubmit={onSubmits}>
+                    <h1>Hello {fullMessage} {LastN}</h1>
 
-                <input 
-                    type='text' 
-                    placeholder='Enter Your Name' 
-                    onChange={inputEvent}
-                    value={message}
-                />
+                    <input 
+                        type='text' 
+                        placeholder='Enter Your Name' 
+                        onChange={inputEvent}
+                        value={message}
+                    />
 
-                <button onClick={onSubmit}>Submit</button>
+                    <input 
+                        type='text' 
+                        placeholder='Enter Your Last Name' 
+                        onChange={inputEventTwo}
+                        value={lastName}
+                    />
+
+                    <button type='submit'>Submit</button>
+
+                </form>
             </div>
         </>
     );
