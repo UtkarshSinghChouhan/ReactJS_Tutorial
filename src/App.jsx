11ce8921ab55cf6a679be27996f43d7194b8bd79
UtkarshSinghChouhan <uscutkarsh9@gmail.com>
2023-03-21 16:24:33 +0530

    useEffect completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,32 @@
-import React, { createContext, useContext } from "react";
-import CompA from "./CompA";
-
-const FirstName = createContext();
-const LastName = createContext();
+import React, { useEffect, useState } from "react";
 
 const App = () => {
+  let [num, setNum] = useState(0);
+  let [nums, setNums] = useState(0);
+
+  //Remember that use effect always expect a function
+  useEffect(() => {
+    alert("clicked");
+  }, [num]);
+
+  const changeNum = () => {
+    setNum(num + 1);
+  };
+
+  const changeNums = () => {
+    setNums(nums + 1);
+  };
+
   return (
     <>
-      <FirstName.Provider value="Utkarsh">
-        <LastName.Provider value="Chouhan">
-          <CompA />
-        </LastName.Provider>
-      </FirstName.Provider>
+      <button onClick={changeNum}>Click me to change the state : {num}</button>
+      <br />
+      <br />
+      <button onClick={changeNums}>
+        Click me to change the state : {nums}
+      </button>
     </>
   );
 };
 
 export default App;
-export { FirstName, LastName };
