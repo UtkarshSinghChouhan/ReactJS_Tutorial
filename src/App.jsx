ac9f1c751d8e59c8d3f72ce55c34a7674aa3f998
UtkarshSinghChouhan <uscutkarsh9@gmail.com>
2023-03-21 16:39:13 +0530

    challenge 7 completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,19 @@ import React, { useEffect, useState } from "react";
 
 const App = () => {
   let [num, setNum] = useState(0);
-  let [nums, setNums] = useState(0);
 
-  //Remember that use effect always expect a function
   useEffect(() => {
-    alert("clicked");
-  }, [num]);
-
-  const changeNum = () => {
-    setNum(num + 1);
-  };
-
-  const changeNums = () => {
-    setNums(nums + 1);
-  };
+    document.title = `Clicked ${num} times`;
+  });
 
   return (
     <>
-      <button onClick={changeNum}>Click me to change the state : {num}</button>
-      <br />
-      <br />
-      <button onClick={changeNums}>
-        Click me to change the state : {nums}
+      <button
+        onClick={() => {
+          setNum(num + 1);
+        }}
+      >
+        Clicked {num} times
       </button>
     </>
   );
