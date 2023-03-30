a881cbc37a426d381cf15574ceacb7b3e8100661
UtkarshSinghChouhan <uscutkarsh9@gmail.com>
2023-03-30 21:44:04 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import Statewise from "./Statewise";
 
 const App = () => {
-  let [num, setNum] = useState(0);
-
-  useEffect(() => {
-    document.title = `Clicked ${num} times`;
-  });
-
   return (
     <>
-      <button
-        onClick={() => {
-          setNum(num + 1);
-        }}
-      >
-        Clicked {num} times
-      </button>
+      <Statewise />
     </>
   );
 };
---
+++ src/Statewise.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Statewise = () => {
+  return (
+    <>
+      <h1> India Covid 19 Dashboard</h1>
+    </>
+  );
+};
+
+export default Statewise;
