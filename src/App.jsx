fd153bdf146b39b640f3cc54ac3650a9815fe1d2
UtkarshSinghChouhan <uscutkarsh9@gmail.com>
2023-07-12 14:01:01 +0530

    in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,18 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
-
- 
 
 const App = () => {
-    let [time, setTime] = useState(new Date().toLocaleTimeString());
 
-    let date = new Date().toDateString();
+  let [time, update] = useState(new Date().toLocaleTimeString());
 
-    setInterval(() => {
-        setTime(new Date().toLocaleTimeString());
-    }, 1000);
 
-    return(
-        <>
-        <div className='container'>
-            <h1 className='heading'> {time} </h1>
-            <h1 className='heading'> {date} </h1>
-        </div>
-            
-        </>
-    )
+  return(
+    <>
+      <div className="container">
+        <h1>{time}</h1>
+      </div>
+    </>
+  )
 }
 
 export default App;
