c9deec8298dd58009baa276cec9937a728387838
UtkarshSinghChouhan <uscutkarsh9@gmail.com>
2023-07-12 13:28:28 +0530

    challenge revision done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import react from 'react';
+import react, { useState } from 'react';
 
 const App = () => {
+
+  let[time, update] = useState(new Date().toLocaleTimeString());
+
+  const updateTime = () => {
+    update(new Date().toLocaleTimeString());
+  }
+
+
   return(
     <>
       <div className="container">
-        
+        <h1>{time}</h1>
+        <button onClick={updateTime}>Update Time</button>
       </div>
     </>
   )
