7ed616493d22f826709063ac2bd547f24d37d601
UtkarshSinghChouhan <uscutkarsh9@gmail.com>
2023-07-12 13:04:47 +0530

    revision done

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,21 @@
 import React, { useState } from "react";
-import './index.css'
-import Header from './Components/Header';
-import CreateNote from './Components/CreateNote';
-import Note from './Components/Note';
-import Footer from './Components/Footer';
 
-const App = () => {
-  let[addItem, setItem] = useState([]);   //addItems represent array of objects
+const App = () =>{
 
+  
 
-  const viewNote = (note) =>{
-    // alert("Clicked");
-    setItem((preVal) => {
-        return[...preVal, note]
-    })
+  const[num, update] = useState(0);
 
-    // console.log(note)
-  }
-
-  const onDelete = (id) => {
-    // console.log("inside App triggered id " + id);
-
-    //preVal is the array of objects
-    setItem((preVal) => {
-      return preVal.filter((ele, idx) => {
-        return idx != id;  //it basically says that return all those objects whose id is not equal to the id of the triggered object. 
-      })
-    })
+  const incre = () => {
+    update(num  + 1);
   }
 
   return(
     <>
-      <Header/>
-      <CreateNote passNote={viewNote}/>
-
-     
-      {addItem.map((ele, idx) => {
-          let{title, content} = ele;
-
-          return <Note 
-            key={idx}
-            id={idx}
-            title={title}
-            content={content}
-            delete={onDelete}
-          />
-      })}
-
-      <Footer/>
+      <div className="container">
+        <h1>{num}</h1>
+        <button onClick={incre}> Click</button>
+      </div>
     </>
   )
 }
