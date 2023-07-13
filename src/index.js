593d132129b664729f26c65a06e6b9c0dea2c385
UtkarshSinghChouhan <uscutkarsh9@gmail.com>
2023-07-13 12:32:35 +0530

    Done

--- src/index.js
+++ src/index.js
@@ -1,9 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+
+const Card = (props) => {
+
+    console.log(props)
+    return(
+        <>
+            <div className="card">
+
+                <img src={props.imgsrc} 
+                    alt="myPic" 
+                    className="card_img"
+                />
 
 
+                <div className="card_info">
 
-ReactDOM.render(<App />,
-    document.getElementById("root")
- );
+                    <div className="card_category">A Netflix Original</div>
+
+                    <h3 className="card_category">{props.title}</h3>
+
+                    <a href="#" target="_blank">
+                        <button>Watch Now</button>
+                    </a>
+
+
+                </div>
+
+            </div>
+
+        </>
+    );
+}
+
+ReactDOM.render(
+    <>
+
+       <Card  
+            imgsrc = "https://www.denofgeek.com/wp-content/uploads/2022/05/stranger-things-season-4-poster.jpeg?resize=768%2C432"
+            title = "Stranger Things"
+       />
+
+      
+            
+      
+       <Card  
+            imgsrc = "https://www.denofgeek.com/wp-content/uploads/2022/05/stranger-things-season-4-poster.jpeg?resize=768%2C432"
+            title = "Stranger Things"
+       />
+
+      
+            
+      
+       
+
+            
+    </>,
+    document.getElementById('root')
+)
