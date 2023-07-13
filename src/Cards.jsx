82f32ef2d6dce5fe9239bbd970c5497d548baff4
UtkarshSinghChouhan <uscutkarsh9@gmail.com>
2023-07-13 12:36:10 +0530

    completed

---
+++ src/Cards.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Card = (props) => {
+
+    console.log(props)
+    return(
+        <>
+            <div className="card">
+
+                <img src={props.imgsrc} 
+                    alt="myPic" 
+                    className="card_img"
+                />
+
+
+                <div className="card_info">
+
+                    <div className="card_category">A Netflix Original</div>
+
+                    <h3 className="card_category">{props.title}</h3>
+
+                    <a href="#" target="_blank">
+                        <button>Watch Now</button>
+                    </a>
+
+
+                </div>
+
+            </div>
+
+        </>
+    );
+}
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -1,37 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const Card = (props) => {
-
-    console.log(props)
-    return(
-        <>
-            <div className="card">
-
-                <img src={props.imgsrc} 
-                    alt="myPic" 
-                    className="card_img"
-                />
+import Card from './Cards.jsx'
 
 
-                <div className="card_info">
-
-                    <div className="card_category">A Netflix Original</div>
-
-                    <h3 className="card_category">{props.title}</h3>
-
-                    <a href="#" target="_blank">
-                        <button>Watch Now</button>
-                    </a>
-
-
-                </div>
-
-            </div>
-
-        </>
-    );
-}
 
 ReactDOM.render(
     <>
